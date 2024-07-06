821bddba53eda5cffee36c606c5e712136d91751
phuc <nguyendaihoangphuc24@gmail.com>
2024-07-06 08:28:34 +0700

    Fix UserService va xoa cac using statements khong can thiet

--- DemoNetApi.Application/Services/UserService.cs
+++ DemoNetApi.Application/Services/UserService.cs
@@ -11,24 +11,21 @@ namespace DemoNetApi.Application.Services
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        private readonly ILogger<UserService> _logger;
-        public UserService(IUserRepository userRepository,IMapper mapper ,ILogger<UserService> logger)
+        public UserService(IUserRepository userRepository,IMapper mapper)
         {
             _userRepository = userRepository;
             _mapper = mapper;
-            _logger = logger;
         }
         public async Task<LoginRespone> LoginUserAsyncService(LoginUser user)
         {
 
             var userlogin = _mapper.Map<LoginUser>(user);
-
             return await _userRepository.LoginUserAsync(userlogin);
         }
 
         public async Task<RegisterRespone> RegisterUserAsyncService(RegisterUser user)
         {
-            var registerUser = _mapper.Map<RegisterUser>(user); 
+            var registerUser = _mapper.Map<RegisterUser>(user);
             return await _userRepository.RegisterUserAsync(registerUser);
         }
     }
